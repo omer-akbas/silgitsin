2b370de36c57221e8aa4a56fd1734f0e55637686
ömer akbaş <omerakbas@gmail.com>
2020-02-21 16:16:11 +0300

    errsend test

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module awesomeProject
 
 go 1.13
+
+require (
+	github.com/omer-akbas/errsend v0.0.2
+	github.com/omer-akbas/gocr v0.0.0-20200217160137-a455033993ec
+)
--- main.go
+++ main.go
@@ -1,21 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/omer-akbas/errsend"
+	"github.com/omer-akbas/gocr"
+	"log"
+)
 
 func main() {
 	ch := make(chan string, 1)
-	process(ch)
+
+	err := process(ch)
+	if err != nil {
+		log.Println("process func err: ", err.Error())
+		e := errsend.ErrSend{To: []string{"izmirlieleman@gmail.com"}, Subject: "Test", Body: err.Error(), IsTelegram:true}
+		_ = e.Send()
+	}
 	fmt.Println(<-ch)
 
 	total := sum(8, 3)
 	fmt.Println("total: ", total)
-}
 
+	err = gocr.ImageToText("./temp/in/1.tif", "./temp/out/test")
+	if err != nil{
+		log.Println("err:", err.Error())
+	}
+}
 
 //Channel icerisindeki string degisir...
 func process (ch chan string) error{
 	ch<-"Sinan"
-	return nil
+	return fmt.Errorf("Cok acayp seyler oluyor...")
 }
 
 func sum(n1, n2 int) int {
