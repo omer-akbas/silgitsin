f7c7d171749e4be6332d526d7f0bce4179807578
ömer akbaş <omerakbas@gmail.com>
2020-02-21 14:34:08 +0300

    channel test

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ func main() {
 	ch := make(chan string, 1)
 	process(ch)
 	fmt.Println(<-ch)
+
+	total := sum(8, 3)
+	fmt.Println("total: ", total)
 }
 
 
@@ -13,4 +16,8 @@ func main() {
 func process (ch chan string) error{
 	ch<-"Sinan"
 	return nil
+}
+
+func sum(n1, n2 int) int {
+	return n1 + n2
 }
